ac373e04b5ee781515056272510b81adbab7d159
JollyDaniel <khodienkod@gmail.com>
2017-05-18 13:51:48 +0300

    First commit

---
+++ Assets/Scripts/Rabit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabit : MonoBehaviour {
+
+	public float speed = 5;
+	Rigidbody2D rBody = null;
+	SpriteRenderer sr = null;
+
+	// Use this for initialization
+	void Start () {
+		rBody = this.GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+	
+	void FixedUpdate() {
+		float value = Input.GetAxis("Horizontal");
+
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = rBody.velocity;
+			vel.x = value * speed;
+			rBody.velocity = vel;
+		}
+		if (value < 0) sr.flipX = true;
+		else if (value > 0) sr.flipX = false;
+	}
+}
